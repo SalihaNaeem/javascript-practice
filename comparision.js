ec3cde3494aaad687a656386e60b1d687f4febaa
SalihaNaeem <salihanaeem58@gmail.com>
2025-06-14 00:01:34 -0700

    comparision practice in javascript is also done

---
+++ comparision.js
@@ -0,0 +1,18 @@
+//console.log(2 > 1);         // true, loose equality
+//console.log(2 >= 1);       // false, strict equality
+//console.log(2 < 1);       // false, loose equality
+//console.log(2 <= 1);      // false, strict equality
+//console.log(2 == 1);      // false, loose equality
+//console.log(2 != 1);      // true, loose inequality
+
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null < 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+console.log("2" === 2);
